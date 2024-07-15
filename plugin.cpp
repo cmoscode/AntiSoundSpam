2cb01e0b6d8537dce1c94be2e294c92f4448508c
sh0tx420 <55631215+sh0tx420@users.noreply.github.com>
2024-07-15 20:56:36 +0300

    Fix project

--- plugin.cpp
+++ plugin.cpp
@@ -1,7 +1,7 @@
 #include <ISvenModAPI.h>
-#include <IClientHooks.h>
-#include <IClientPlugin.h>
 #include <interface.h>
+#include <IClientPlugin.h>
+#include <IClientHooks.h>
 
 #include <convar.h>
 #include <dbg.h>
@@ -39,77 +39,76 @@ ConVar snd_antispam_remove_delay( "snd_antispam_remove_delay", "60", FCVAR_CLIEN
 //-----------------------------------------------------------------------------
 // Plugin interface
 //-----------------------------------------------------------------------------
-
 class CAntiSoundSpam : public IClientPlugin, IClientHooks
 {
 public:
 	CAntiSoundSpam();
 
 	// IClientHooks interface
-	virtual HOOK_RESULT HUD_VidInit( void );
-	virtual HOOK_RESULT HUD_Redraw( float time, int intermission );
-	virtual HOOK_RESULT HOOK_RETURN_VALUE HUD_UpdateClientData( int *changed, client_data_t *pcldata, float flTime );
-	virtual HOOK_RESULT HUD_PlayerMove( playermove_t *ppmove, int server );
-	virtual HOOK_RESULT IN_ActivateMouse( void );
-	virtual HOOK_RESULT IN_DeactivateMouse( void );
-	virtual HOOK_RESULT IN_MouseEvent( int mstate );
-	virtual HOOK_RESULT IN_ClearStates( void );
-	virtual HOOK_RESULT IN_Accumulate( void );
-	virtual HOOK_RESULT CL_CreateMove( float frametime, usercmd_t *cmd, int active );
-	virtual HOOK_RESULT HOOK_RETURN_VALUE CL_IsThirdPerson( int *thirdperson );
-	virtual HOOK_RESULT HOOK_RETURN_VALUE KB_Find( kbutton_t **button, const char *name );
-	virtual HOOK_RESULT CAM_Think( void );
-	virtual HOOK_RESULT V_CalcRefdef( ref_params_t *pparams );
-	virtual HOOK_RESULT HOOK_RETURN_VALUE HUD_AddEntity( int *visible, int type, cl_entity_t *ent, const char *modelname );
-	virtual HOOK_RESULT HUD_CreateEntities( void );
-	virtual HOOK_RESULT HUD_DrawNormalTriangles( void );
-	virtual HOOK_RESULT HUD_DrawTransparentTriangles( void );
-	virtual HOOK_RESULT HUD_StudioEvent( const mstudioevent_t *studio_event, const cl_entity_t *entity );
-	virtual HOOK_RESULT HUD_PostRunCmd( local_state_t *from, local_state_t *to, usercmd_t *cmd, int runfuncs, double time, unsigned int random_seed );
-	virtual HOOK_RESULT HUD_TxferLocalOverrides( entity_state_t *state, const clientdata_t *client );
-	virtual HOOK_RESULT HUD_ProcessPlayerState( entity_state_t *dst, const entity_state_t *src );
-	virtual HOOK_RESULT HUD_TxferPredictionData( entity_state_t *ps, const entity_state_t *pps, clientdata_t *pcd, const clientdata_t *ppcd, weapon_data_t *wd, const weapon_data_t *pwd );
-	virtual HOOK_RESULT Demo_ReadBuffer( int size, unsigned const char *buffer );
-	virtual HOOK_RESULT HOOK_RETURN_VALUE HUD_ConnectionlessPacket( int *valid_packet, netadr_t *net_from, const char *args, const char *response_buffer, int *response_buffer_size );
-	virtual HOOK_RESULT HOOK_RETURN_VALUE HUD_GetHullBounds( int *hullnumber_exist, int hullnumber, float *mins, float *maxs );
-	virtual HOOK_RESULT HUD_Frame( double time );
-	virtual HOOK_RESULT HOOK_RETURN_VALUE HUD_Key_Event( int *process_key, int down, int keynum, const char *pszCurrentBinding );
-	virtual HOOK_RESULT HUD_TempEntUpdate( double frametime, double client_time, double cl_gravity, TEMPENTITY **ppTempEntFree, TEMPENTITY **ppTempEntActive, int ( *Callback_AddVisibleEntity )( cl_entity_t *pEntity ), void ( *Callback_TempEntPlaySound )( TEMPENTITY *pTemp, float damp ) );
-	virtual HOOK_RESULT HOOK_RETURN_VALUE HUD_GetUserEntity( cl_entity_t **ent, int index );
-	virtual HOOK_RESULT HUD_VoiceStatus( int entindex, qboolean bTalking );
-	virtual HOOK_RESULT HUD_DirectorMessage( unsigned char command, unsigned int firstObject, unsigned int secondObject, unsigned int flags );
-	virtual HOOK_RESULT HUD_ChatInputPosition( int *x, int *y );
+	virtual HOOK_RESULT HUD_VidInit(void);
+	virtual HOOK_RESULT HUD_Redraw(float time, int intermission);
+	virtual HOOK_RESULT HOOK_RETURN_VALUE HUD_UpdateClientData(int *changed, client_data_t *pcldata, float flTime);
+	virtual HOOK_RESULT HUD_PlayerMove(playermove_t *ppmove, int server);
+	virtual HOOK_RESULT IN_ActivateMouse(void);
+	virtual HOOK_RESULT IN_DeactivateMouse(void);
+	virtual HOOK_RESULT IN_MouseEvent(int mstate);
+	virtual HOOK_RESULT IN_ClearStates(void);
+	virtual HOOK_RESULT IN_Accumulate(void);
+	virtual HOOK_RESULT CL_CreateMove(float frametime, usercmd_t *cmd, int active);
+	virtual HOOK_RESULT HOOK_RETURN_VALUE CL_IsThirdPerson(int *thirdperson);
+	virtual HOOK_RESULT HOOK_RETURN_VALUE KB_Find(kbutton_t **button, const char *name);
+	virtual HOOK_RESULT CAM_Think(void);
+	virtual HOOK_RESULT V_CalcRefdef(ref_params_t *pparams);
+	virtual HOOK_RESULT HOOK_RETURN_VALUE HUD_AddEntity(int *visible, int type, cl_entity_t *ent, const char *modelname);
+	virtual HOOK_RESULT HUD_CreateEntities(void);
+	virtual HOOK_RESULT HUD_DrawNormalTriangles(void);
+	virtual HOOK_RESULT HUD_DrawTransparentTriangles(void);
+	virtual HOOK_RESULT HUD_StudioEvent(const mstudioevent_t *studio_event, const cl_entity_t *entity);
+	virtual HOOK_RESULT HUD_PostRunCmd(local_state_t *from, local_state_t *to, usercmd_t *cmd, int runfuncs, double time, unsigned int random_seed);
+	virtual HOOK_RESULT HUD_TxferLocalOverrides(entity_state_t *state, const clientdata_t *client);
+	virtual HOOK_RESULT HUD_ProcessPlayerState(entity_state_t *dst, const entity_state_t *src);
+	virtual HOOK_RESULT HUD_TxferPredictionData(entity_state_t *ps, const entity_state_t *pps, clientdata_t *pcd, const clientdata_t *ppcd, weapon_data_t *wd, const weapon_data_t *pwd);
+	virtual HOOK_RESULT Demo_ReadBuffer(int size, unsigned const char *buffer);
+	virtual HOOK_RESULT HOOK_RETURN_VALUE HUD_ConnectionlessPacket(int *valid_packet, netadr_t *net_from, const char *args, const char *response_buffer, int *response_buffer_size);
+	virtual HOOK_RESULT HOOK_RETURN_VALUE HUD_GetHullBounds(int *hullnumber_exist, int hullnumber, float *mins, float *maxs);
+	virtual HOOK_RESULT HUD_Frame(double time);
+	virtual HOOK_RESULT HOOK_RETURN_VALUE HUD_Key_Event(int *process_key, int down, int keynum, const char *pszCurrentBinding);
+	virtual HOOK_RESULT HUD_TempEntUpdate(double frametime, double client_time, double cl_gravity, TEMPENTITY **ppTempEntFree, TEMPENTITY **ppTempEntActive, int (*Callback_AddVisibleEntity)(cl_entity_t *pEntity), void (*Callback_TempEntPlaySound)(TEMPENTITY *pTemp, float damp));
+	virtual HOOK_RESULT HOOK_RETURN_VALUE HUD_GetUserEntity(cl_entity_t **ent, int index);
+	virtual HOOK_RESULT HUD_VoiceStatus(int entindex, qboolean bTalking);
+	virtual HOOK_RESULT HUD_DirectorMessage(unsigned char command, unsigned int firstObject, unsigned int secondObject, unsigned int flags);
+	virtual HOOK_RESULT HUD_ChatInputPosition(int *x, int *y);
 
 	// IClientPlugin interface
 	virtual api_version_t GetAPIVersion();
 
-	virtual bool Load( CreateInterfaceFn pfnSvenModFactory, ISvenModAPI *pSvenModAPI, IPluginHelpers *pPluginHelpers );
-	virtual void PostLoad( bool bGlobalLoad );
-	virtual void Unload( void );
-	virtual bool Pause( void );
-	virtual void Unpause( void );
+	virtual bool Load(CreateInterfaceFn pfnSvenModFactory, ISvenModAPI *pSvenModAPI, IPluginHelpers *pPluginHelpers);
+	virtual void PostLoad(bool bGlobalLoad);
+	virtual void Unload(void);
+	virtual bool Pause(void);
+	virtual void Unpause(void);
 
-	virtual void OnFirstClientdataReceived( client_data_t *pcldata, float flTime );
-	virtual void OnBeginLoading( void );
-	virtual void OnEndLoading( void );
-	virtual void OnDisconnect( void );
+	virtual void OnFirstClientdataReceived(client_data_t *pcldata, float flTime);
+	virtual void OnBeginLoading(void);
+	virtual void OnEndLoading(void);
+	virtual void OnDisconnect(void);
 
-	virtual void GameFrame( client_state_t state, double frametime, bool bPostRunCmd );
+	virtual void GameFrame(client_state_t state, double frametime, bool bPostRunCmd);
 
-	virtual void Draw( void );
-	virtual void DrawHUD( float time, int intermission );
+	virtual void Draw(void);
+	virtual void DrawHUD(float time, int intermission);
 
-	virtual const char *GetName( void );
-	virtual const char *GetAuthor( void );
-	virtual const char *GetVersion( void );
-	virtual const char *GetDescription( void );
-	virtual const char *GetURL( void );
-	virtual const char *GetDate( void );
-	virtual const char *GetLogTag( void );
+	virtual const char *GetName(void);
+	virtual const char *GetAuthor(void);
+	virtual const char *GetVersion(void);
+	virtual const char *GetDescription(void);
+	virtual const char *GetURL(void);
+	virtual const char *GetDate(void);
+	virtual const char *GetLogTag(void);
 
 private:
-	static bool HashMap_Compare( const anti_sound_spam_t &a, const anti_sound_spam_t &b ) { return ( a.entindex == b.entindex && strcmp( a.sound, b.sound ) == 0 ); }
-	static unsigned int HashMap_Hash( const anti_sound_spam_t &a ) { return HashStringCaseless( a.sound ); }
+	static bool HashMap_Compare(const anti_sound_spam_t &a, const anti_sound_spam_t &b) { return (a.entindex == b.entindex && strcmp(a.sound, b.sound) == 0); }
+	static unsigned int HashMap_Hash(const anti_sound_spam_t &a) { return HashStringCaseless(a.sound); }
 
 private:
 	CHash<anti_sound_spam_t> m_SoundsList;
